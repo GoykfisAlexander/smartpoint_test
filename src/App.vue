fa472ccaba6fe25ec12ae084469b0048008f26da
GoykfisAlexander <agoykhfis@gmail.com>
2023-09-29 22:30:57 +0300

    smartpoint-test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Модальное окно -->
+    <AddCityModal v-if="showModal" />
+
+    <h1>Список городов</h1>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Город</th>
+          <th scope="col">Количество жителей</th>
+          <th scope="col">Количество автомобилей</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(city, index) in cities" :key="index">
+          <td>{{ city.name }}</td>
+          <td>{{ city.population }}</td>
+          <td>{{ city.cars }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="changeShowModal" class="btn btn-primary">
+      Добавить город
+    </button>
+    <!-- Графики -->
+    <CityCharts />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddCityModal from "./components/AddCityModal.vue";
+import CityCharts from "./components/CityCharts.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AddCityModal,
+    CityCharts,
+  },
+
+  computed: {
+    cities() {
+      return this.$store.state.cities;
+    },
+    showModal() {
+      return this.$store.state.showModal;
+    },
+  },
+
+  methods: {
+    changeShowModal() {
+      this.$store.commit("changeShowModal");
+    },
+  },
+};
 </script>
 
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
-import { createStore } from 'vuex'
+import axios from "axios";
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-  },
-  getters: {
+    cities: [],
+    showModal: false,
   },
   mutations: {
+    setCities(state, cities) {
+      state.cities = cities;
+    },
+    changeShowModal(state) {
+      state.showModal = !state.showModal;
+    },
+    addCity(state, newCity) {
+      state.cities.push(newCity);
+    },
   },
   actions: {
+    async fetchCities({ commit }) {
+      try {
+        const response = await axios.get("./data.json");
+        const cities = response.data.cities;
+        commit("setCities", cities);
+      } catch (error) {
+        console.error("Ошибка при загрузке данных о городах:", error);
+      }
+    },
   },
-  modules: {
-  }
-})
+  getters: {},
+});
